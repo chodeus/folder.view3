93d9a5c766017c1ceaf0a69faef9665680917b80
chodeus <190988615+chodeus@users.noreply.github.com>
2026-09-13 14:19:04 +0800

    fix: require an id on update.php

--- src/folder.view3/usr/local/emhttp/plugins/folder.view3/server/update.php
+++ src/folder.view3/usr/local/emhttp/plugins/folder.view3/server/update.php
@@ -2,6 +2,6 @@
     require_once("/usr/local/emhttp/plugins/folder.view3/server/lib.php");
     fv3_post_init();
     $type = fv3_validate_type($_POST['type'] ?? '');
-    // updateFolder() owns the id rule
-    updateFolder($type, fv3_post_string('content'), fv3_post_string('id'));
+    // A missing id is refused (create.php is the endpoint that makes new folders); updateFolder() owns the rule for its value
+    updateFolder($type, fv3_post_string('content'), fv3_post_required('id'));
 ?>
